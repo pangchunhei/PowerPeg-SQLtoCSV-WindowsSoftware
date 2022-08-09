874ee48018bc6e04f2b6a945f7305856be7a75b8
Elston Pang <elstonpang2000@gmail.com>
2022-08-09 16:33:42 +0800

    Update Instant Generation UI

---
+++ project/PowerPeg-SQL-to-CSV/App-UI/InstantGenerationOptionForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace App_UI
+{
+    partial class InstantGenerationOptionForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "headerLabel";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(40, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Instant Generation";
+            // 
+            // label2
+            // 
+            this.label2.AccessibleName = "header2Label";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(80, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Server Information: ";
+            // 
+            // label3
+            // 
+            this.label3.AccessibleName = "serverInfoLabel";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(314, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "<Server Address>";
+            // 
+            // label4
+            // 
+            this.label4.AccessibleName = "gernerationOptionLabel";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(80, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(229, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Generation Options: ";
+            // 
+            // label5
+            // 
+            this.label5.AccessibleName = "fromDateLabel";
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(124, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "From: ";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.AccessibleName = "fromDateCal";
+            this.monthCalendar1.Location = new System.Drawing.Point(203, 219);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AccessibleName = "toDateLabel";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(539, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 28);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "To:";
+            // 
+            // monthCalendar2
+            // 
+            this.monthCalendar2.AccessibleName = "toDateCal";
+            this.monthCalendar2.Location = new System.Drawing.Point(587, 219);
+            this.monthCalendar2.Name = "monthCalendar2";
+            this.monthCalendar2.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AccessibleName = "selectFieldLabel";
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(124, 445);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 28);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Select Field: ";
+            // 
+            // listView1
+            // 
+            this.listView1.AccessibleName = "selectFieldListView";
+            this.listView1.CheckBoxes = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.Location = new System.Drawing.Point(250, 445);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(522, 262);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label8
+            // 
+            this.label8.AccessibleName = "outputLoactionLabel";
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(124, 728);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(164, 28);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Output Location: ";
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "generateBtn";
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(430, 789);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label9
+            // 
+            this.label9.AccessibleName = "filePathLabel";
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(434, 731);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 25);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "<Path>";
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "getFileExplorerBtn";
+            this.button2.Location = new System.Drawing.Point(294, 729);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 29);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Open Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // InstantGenerationOptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(993, 867);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.monthCalendar2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "InstantGenerationOptionForm";
+            this.Text = "InstantGenerationOptionForm";
+            this.Load += new System.EventHandler(this.InstantGenerationOptionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private MonthCalendar monthCalendar1;
+        private Label label6;
+        private MonthCalendar monthCalendar2;
+        private Label label7;
+        private ListView listView1;
+        private Label label8;
+        private Button button1;
+        private Label label9;
+        private Button button2;
+    }
+}
---
+++ project/PowerPeg-SQL-to-CSV/App-UI/InstantGenerationOptionForm.cs
@@ -0,0 +1,20 @@
+namespace App_UI
+{
+    public partial class InstantGenerationOptionForm : Form
+    {
+        public InstantGenerationOptionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InstantGenerationOptionForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
