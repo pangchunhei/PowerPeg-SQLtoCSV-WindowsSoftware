069131034f2628ae635d9089442cd155731d3f39
Elston <elstonpang2000@gmail.com>
2022-08-30 14:02:35 +0800

    Add debug console windows 20220830

--- project/PowerPeg-SQL-to-CSV/App-UI/Program.cs
+++ project/PowerPeg-SQL-to-CSV/App-UI/Program.cs
@@ -18,6 +18,9 @@ namespace App_UI
         [STAThread]
         static void Main()
         {
+            NativeMethods.AllocConsole();
+            Console.WriteLine("Debug Console");
+
             //Log stuff
             log.Info("UI Application starting.");
 
@@ -28,6 +31,35 @@ namespace App_UI
             Application.Run(new HomeForm());
 
             log.Info("UI Application ending.");
+
+            NativeMethods.FreeConsole();
         }
+    }
+
+    internal static class NativeMethods
+    {
+        // http://msdn.microsoft.com/en-us/library/ms681944(VS.85).aspx
+        /// <summary>
+        /// Allocates a new console for the calling process.
+        /// </summary>
+        /// <returns>nonzero if the function succeeds; otherwise, zero.</returns>
+        /// <remarks>
+        /// A process can be associated with only one console,
+        /// so the function fails if the calling process already has a console.
+        /// </remarks>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern int AllocConsole();
+
+        // http://msdn.microsoft.com/en-us/library/ms683150(VS.85).aspx
+        /// <summary>
+        /// Detaches the calling process from its console.
+        /// </summary>
+        /// <returns>nonzero if the function succeeds; otherwise, zero.</returns>
+        /// <remarks>
+        /// If the calling process is not already attached to a console,
+        /// the error code returned is ERROR_INVALID_PARAMETER (87).
+        /// </remarks>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern int FreeConsole();
     }
 }
